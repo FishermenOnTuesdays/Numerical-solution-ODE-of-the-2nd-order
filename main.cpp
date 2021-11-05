1f868b7749dd812088d7a4d2f7737b49b249cb09
rouol <karrauln@yandex.ru>
2021-11-06 00:19:47 +0300

    added code and configs for export to Python as lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 ﻿#include "SecondOrderODESolver.h"
 #include <iostream>
 
+using StringPair = std::pair<std::string, std::string>;
+
 int main()
 {
 	//ODE is y'' + cos(x)·y' + sin(x)·y = 1 - cos(x) - sin(x)
@@ -8,13 +10,7 @@ int main()
 	//-y'(0) + y(0) = 0
 	//0·y'(1) + y(1) = 1.3818 ,
 	//where x∈[0,1]
-	FunctionParser p;
-	FunctionParser q;
-	FunctionParser phi;
-	p.Parse("cos(x)", "x");
-	q.Parse("sin(x)", "x");
-	phi.Parse("1 - cos(x) - sin(x)", "x");
-	std::vector<FunctionParser> functions = { p, q, phi };
+	std::vector<StringPair> functions = { StringPair("cos(x)", "x"), StringPair("sin(x)", "x"), StringPair("1 - cos(x) - sin(x)", "x") };
 	Eigen::Matrix<double, 2, 3> boundaries;
 	boundaries << -1, 1, 0,
 				   0, 1, 1.3818;
